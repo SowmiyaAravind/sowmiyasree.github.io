b1c848fc4ef7b2e79c99d1792bbb7a8745b29ff6
SowmiyaAravind <136678849+SowmiyaAravind@users.noreply.github.com>
2025-11-17 21:50:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -13,17 +13,3 @@ window.addEventListener('scroll', () => {
     });
 });
 
-// AUTO-FETCH GITHUB PROJECTS
-fetch("https://api.github.com/users/sowmiyaaravind/repos")
-.then(res => res.json())
-.then(data => {
-    let container = document.getElementById("github-projects");
-    data.slice(0,4).forEach(repo => {
-        container.innerHTML += `
-        <div class="github-card">
-            <h3>${repo.name}</h3>
-            <p>${repo.description || "No description available."}</p>
-            <a href="${repo.html_url}" target="_blank">🔗 View on GitHub</a>
-        </div>`;
-    });
-});
